47a29308114b193f6772e1b32620db5894888b82
Rod Nolan <rnolan@aquent.com>
2023-10-18 10:54:15 -0400

    first iteration of the feature detection approach

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-try {
+// try {
   platformBrowserDynamic().bootstrapModule(AppModule)
     .catch(err => console.error(err));
-} catch (e) {
-  window.location.href = "./unsupported.html";
-}
+// } catch (e) {
+//   window.location.href = "./unsupported.html";
+// }
