926157aeb8c1341a4fe9e98bedf32095b9d26729
Rod Nolan <rnolan@aquent.com>
2023-10-27 15:18:01 -0400

    reorganize the functions to ensure the angular scripts are NOT loaded prematurely

--- index-html-transformer.ts
+++ index-html-transformer.ts
@@ -2,9 +2,7 @@ import { TargetOptions } from '@angular-builders/custom-webpack';
 
 function extractScriptsSrc(inputString: string, startString: string = '<script src="', endString: string = '" type="module"></script>') {
 
-  // const inputString: string = "This is a [start]first[end] example and a [start]second[end] example.";
-
-  // Create a regular expression pattern with the global 'g' flag
+  // Create a regular expression pattern with the global 'g' flag so that all matches are found
   const pattern: RegExp = new RegExp(`${escapeRegExp(startString)}(.*?)${escapeRegExp(endString)}`, 'g');
 
   // Function to escape special characters in a string to use in a regular expression
@@ -15,35 +13,31 @@ function extractScriptsSrc(inputString: string, startString: string = '<script s
   // Use the RegExp exec method in a loop to find all matching substrings
   const matches: string[] = [];
   let match: RegExpExecArray | null;
-
   while ((match = pattern.exec(inputString)) !== null) {
     matches.push(match[1]);
   }
 
-  if (matches.length > 0) {
-    console.log("Extracted strings:", matches)
-  } else {
-    console.log("No matches found.");
-  }
   return matches;
 }
 
 export default (targetOptions: TargetOptions, indexHtml: string) => {
-  console.log("before\n"+ indexHtml);
+  console.log("\nexcerpt of index.html before processing:\n"+ indexHtml.slice(-400));
   var scripts = extractScriptsSrc(indexHtml);
-  console.log(scripts);
   var startIndex = indexHtml.indexOf('<script src="');
   var endIndex = indexHtml.lastIndexOf("</body>");
   var magic = `<script>
-      document.addEventListener("DOMContentLoaded", () => loadAngular(${JSON.stringify(scripts)}));
+      document.addEventListener("DOMContentLoaded", () => storeAngularScripts(${JSON.stringify(scripts)}));
     </script>`;
-  return `${indexHtml.slice(0, startIndex)}
+  var transformedIndexHtml = `${indexHtml.slice(0, startIndex)}
     ${magic}
     ${indexHtml.slice(endIndex)}`;
+
+    console.log("\nexcerpt of index.html after processing:\n"+transformedIndexHtml.slice(-400));
+    return transformedIndexHtml;
 };
 
 /*
-
+this is what the tail end of the file looks like with the default angular builder
 <script src="runtime.1b5af9e61ae2ff40.js" type="module"></script>
 <script src="polyfills.0e1ef76a6fbf2c82.js" type="module"></script>
 <script src="main.15760814d6906cc0.js" type="module"></script>
